d12ee98e62e55c423a30ff40fbb140edcc63b784
Josh Fishbein <joshua.p.fishbein@gmail.com>
2026-02-23 00:00:51 -0500

    Add DEC eda scripts, feature generation scripts, and output csv files

--- scripts/create_dec_features.py
+++ scripts/create_dec_features.py
@@ -11,120 +11,137 @@ Usage:
 """
 
 import os
+import json
 import argparse
 import pandas as pd
-from typing import Dict
-from functools import partial
+from typing import Optional, List
 import numpy as np
 from datetime import timedelta
-import warnings
 
-# Suppress noisy warnings
-warnings.filterwarnings("ignore", message=".*Mean of empty slice.*")
-
-
-TARGET_CSV_PATH = "data/unified_csvs/vct_unified.csv"
+# Input data file paths
+TARGET_CSV_PATH = "data/unified_csvs/vct_unified_prepped.csv"
 FEATURE_CSV_PATH = "data/unified_csvs/vt_dec.csv"
-DEC_STATION_LOCATIONS_CSV = "data/data_dictionaries/DEC_station_locations.csv"
+VCT_TO_DEC_SITE_MAPPING_PATH = "data/data_dictionaries/VCT_to_DEC_site_mappings.json"
+
+# Lookback window to use for aggregating features when pairing with the target observation dates
 AGGREGATION_WINDOW_START = timedelta(days=7)
 AGGREGATION_WINDOW_END = timedelta(days=1)
 
+# Feature columns to store in the final report. These were manually selected according to their feature
+# importance and availability of observations.
+FEATURE_COLUMNS_OF_INTEREST = [
+    "total nitrogen",
+    "total phosphorus",
+    "dissolved phosphorus",
+    "chlorophyll-a",
+    "secchi depth",
+    "temperature",
+]
 
-def create_dec_features(dst: str) -> None:
-    """Create DEC features per target row through data aggregations."""
 
-    def aggregate_features_weekly(
-        station_mapping: Dict[str, str],
-        pivoted_features: pd.DataFrame,
-        target_row: pd.Series,
-    ) -> pd.Series:
-        """Use weekly interval prior to each target observation and  average reading for each feature."""
-        target_report_date = target_row["REPORTDATE"]
-        target_station = target_row["STATION"]
-        # If the report date or station is invalid, return an empty row
-        if pd.isnull(target_report_date) or pd.isnull(target_station):
-            return pd.Series(index=pivoted_features.columns, data=np.nan)
-        # Generate weekly averages of each feature
-        nearest_dec_station = station_mapping[target_station]
-        window_start = str(target_report_date - AGGREGATION_WINDOW_START)
-        window_end = str(target_report_date - AGGREGATION_WINDOW_END)
-        # Use np.nanmean to ignore nan values when generating averages
-        weekly_averages = np.nanmean(
-            pivoted_features.loc[
-                nearest_dec_station, window_start:window_end, :
-            ].values,
-            axis=0,
-        )
-        return pd.Series(index=pivoted_features.columns, data=weekly_averages)
+def create_dec_features(
+    features_to_use: List[str] = FEATURE_COLUMNS_OF_INTEREST, dst: Optional[str] = None
+) -> pd.DataFrame:
+    """Create DEC features per target row through data aggregations.
 
+    Raw features are stored as rows of single test results across all tests and monitoring sites. To convert
+    these into usable features, these must be pivoted so that each test is its own column. The test results
+    must then be aggregated across a window so that they can be correctly paired with target observations.
+
+    Args:
+        features_to_use: only save features included in this list
+        dst: output file destination. If empty, no output file is saved
+    Returns:
+        Feature dataframe
+    """
     target_df = _get_targets(TARGET_CSV_PATH)
     raw_feature_df = _get_dec_features(FEATURE_CSV_PATH)
-    dec_site_locations_df = pd.read_csv(DEC_STATION_LOCATIONS_CSV)
-    vct_to_dec_station_mapping = _map_vct_stations_to_nearest_dec_station(
-        target_df, dec_site_locations_df
-    )
+    with open(VCT_TO_DEC_SITE_MAPPING_PATH, "r") as f:
+        vct_to_dec_site_mapping = json.load(f)
 
     # The feature dataframe has a row per measurement and we need a row per station and measurement date
     # with the features pivoted as columns. We average by the date here so that we don't overweight
     # measurements that occur multiple times per day.
-    # TODO: Figure out if we should be aggregate tests across all depths and stratum
-    # raw_feature_df["Test"] = raw_feature_df.apply(lambda row: f"{row['Test']}_{row['Stratum']}_{row['Depth']}", axis=1)
     pivoted_feature_df = raw_feature_df.pivot_table(
-        index=["Station", "Date"], columns="Test", values="Result", aggfunc=np.nanmean
+        index=["station", "date"], columns="test", values="result", aggfunc=np.nanmean
+    ).sort_index()
+    # Standardize the column names with lowercase and subset to the user requested feature list.
+    pivoted_feature_df.columns = [c.lower() for c in pivoted_feature_df.columns]
+    pivoted_feature_df = pivoted_feature_df[features_to_use]
+
+    # To align our features with our target observations, we create a dataframe from our targets that replicates
+    # each row so that we can easily marge on our feature dataframe for the entire window. We then later group
+    # by the target observation date to aggregate all feature rows across the window.
+    target_df["feature_window"] = target_df["report_date"].apply(
+        lambda dt: pd.date_range(
+            start=dt - AGGREGATION_WINDOW_START,
+            end=dt - AGGREGATION_WINDOW_END,
+            inclusive="both",
+            freq="D",
+        )
     )
-    # Aggregate the features into weekly windows by station
-    aggregated_features = target_df[["STATION", "REPORTDATE"]].apply(
-        partial(
-            aggregate_features_weekly, vct_to_dec_station_mapping, pivoted_feature_df
-        ),
-        axis=1,
+    target_df_exploded_by_window = target_df.explode("feature_window").reset_index(
+        drop=True
     )
-    aggregated_features.to_csv(dst, index=False)
-    os.chmod(
-        dst, 0o777
-    )  # Open up all the file permissions (read/write/execute for all)
+
+    # Iterate over each target monitoring site and aggregate the features over the interval for each corresponding
+    # feature monitoring site. If multiple feature monitoring sites map to the target monitoring site, coalesce
+    # the datasets to minimize the number of null entries.
+    features_aggregated_weekly = []
+    for target_site, grp in target_df_exploded_by_window.groupby("region"):
+        # Get the list of matching feature sites
+        feature_sites = [
+            row["DEC sites"]
+            for row in vct_to_dec_site_mapping
+            if row["VCT site"] == target_site
+        ][0]
+        feature_df_for_target_site = pd.DataFrame()
+        for matching_dec_site in feature_sites:
+            # Subselect the feature rows to match the target site
+            feature_rows_for_target_site = pivoted_feature_df.loc[matching_dec_site]
+            # Merge with the target dataframe using the window
+            target_merged_with_features = grp.merge(
+                feature_rows_for_target_site,
+                left_on="feature_window",
+                right_index=True,
+                how="left",
+            ).drop(columns=["feature_window"])
+            # Aggregate across the window using a mean
+            weekly_avg_df = target_merged_with_features.groupby(
+                ["region", "report_date"]
+            ).mean()
+            if feature_df_for_target_site.empty:
+                feature_df_for_target_site = weekly_avg_df
+                continue
+            # If multiple sites can be mapped, coalesce null values
+            feature_df_for_target_site = feature_df_for_target_site.fillna(
+                weekly_avg_df
+            )
+        features_aggregated_weekly.append(feature_df_for_target_site)
+    all_features = pd.concat(features_aggregated_weekly).reset_index()
+    if dst is not None:  # Optionally save the results to disk
+        all_features.to_csv(dst, index=False)
+        os.chmod(
+            dst, 0o777
+        )  # Open up all the file permissions (read/write/execute for all)
+    return all_features
 
 
 def _get_targets(src: str) -> pd.DataFrame:
-    target_df = pd.read_csv(src).astype({"LATITUDE": float, "LONGITUDE": float})
-    target_df["REPORTDATE"] = pd.to_datetime(
-        target_df["REPORTDATE"], format="%m/%d/%Y"
+    target_df = pd.read_csv(src).astype({"latitude": float, "longitude": float})
+    target_df["report_date"] = pd.to_datetime(
+        target_df["report_date"], format="%m/%d/%Y"
     ).dt.date
-    return target_df
+    return target_df[["region", "report_date"]]
 
 
 def _get_dec_features(src):
     feature_df = pd.read_csv(src)
-    feature_df["Date"] = pd.to_datetime(feature_df["Date"], format="%m-%d-%Y")
+    feature_df.columns = [c.lower() for c in feature_df.columns]
+    feature_df["date"] = pd.to_datetime(feature_df["date"], format="%m-%d-%Y")
+    # Drop the first 5 characters of the station name which are the numeric code
+    feature_df["station"] = feature_df["station"].str[5:]
     return feature_df
-
-
-def _map_vct_stations_to_nearest_dec_station(
-    target_df: pd.DataFrame, dec_site_locations_df: pd.DataFrame
-) -> Dict[str, str]:
-    """Generate a dictionary mapping of target stations to DEC stations using a euclidean distance"""
-
-    def get_nearest_dec_station(dec_station_locations, target_row):
-        distances = (
-            (dec_station_locations["degrees_latitude"] - target_row["LATITUDE"]) ** 2
-            + (dec_station_locations["degrees_longitude"] - target_row["LONGITUDE"])
-            ** 2
-        ) ** (1 / 2)
-        return dec_station_locations.loc[distances.idxmin()]["DEC_station"]
-
-    target_df_stations = (
-        target_df[["STATION", "LATITUDE", "LONGITUDE"]]
-        .groupby("STATION")
-        .agg("mean")
-        .reset_index()
-    )
-    target_df_stations = target_df_stations.drop_duplicates()
-    target_df_stations["NEAREST_DEC_STATION"] = target_df_stations.apply(
-        partial(get_nearest_dec_station, dec_site_locations_df), axis=1
-    )
-    return dict(
-        zip(target_df_stations["STATION"], target_df_stations["NEAREST_DEC_STATION"])
-    )
 
 
 def main():
@@ -134,7 +151,6 @@ def main():
     parser.add_argument(
         "-o", action="store", default="out.csv", help="Output file name"
     )
-
     args = parser.parse_args()
     create_dec_features(dst=args.o)
 
